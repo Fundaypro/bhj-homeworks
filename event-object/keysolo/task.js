062bd6c06304586ff8eb066e7ecc7040ea613f21
Fundaypro <fundaypro123@gmail.com>
2021-10-28 16:38:08 +0200

    some add

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -1,90 +1,108 @@
-class Game {
-  constructor(container) {
-    this.container = container;
-    this.wordElement = container.querySelector('.word');
-    this.winsElement = container.querySelector('.status__wins');
-    this.lossElement = container.querySelector('.status__loss');
-
-    this.reset();
-
-    this.registerEvents();
-  }
-
-  reset() {
-    this.setNewWord();
-    this.winsElement.textContent = 0;
-    this.lossElement.textContent = 0;
-  }
-
-  registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
-  }
-
-  success() {
-    this.currentSymbol.classList.add('symbol_correct');
-    this.currentSymbol = this.currentSymbol.nextElementSibling;
-    if (this.currentSymbol !== null) {
-      return;
-    }
-
-    if (++this.winsElement.textContent === 10) {
-      alert('Победа!');
-      this.reset();
-    }
-    this.setNewWord();
-  }
-
-  fail() {
-    if (++this.lossElement.textContent === 5) {
-      alert('Вы проиграли!');
-      this.reset();
-    }
-    this.setNewWord();
-  }
-
-  setNewWord() {
-    const word = this.getWord();
-
-    this.renderWord(word);
-  }
-
-  getWord() {
-    const words = [
-        'bob',
-        'awesome',
-        'netology',
-        'hello',
-        'kitty',
-        'rock',
-        'youtube',
-        'popcorn',
-        'cinema',
-        'love',
-        'javascript'
-      ],
-      index = Math.floor(Math.random() * words.length);
-
-    return words[index];
-  }
-
-  renderWord(word) {
-    const html = [...word]
-      .map(
-        (s, i) =>
-          `<span class="symbol ${i === 0 ? 'symbol_current': ''}">${s}</span>`
-      )
-      .join('');
-    this.wordElement.innerHTML = html;
-
-    this.currentSymbol = this.wordElement.querySelector('.symbol_current');
+class KeyboardSoloGame {
+  constructor() {
+    this.gameContainer = document.getElementById("keyboard_solo_game");
+    this.interface = {
+      writeCounter: {
+        element: (() => {
+          const newEl = document.createElement("p");
+          newEl.textContent = "Правильно введённых слов : ";
+          const number = document.createElement("span");
+          number.innerText = 0;
+          newEl.append(number);
+          this.gameContainer.append(newEl);
+          return number;
+        })(),
+        count: 0,
+        upNum() {
+          this.element.innerText = this.count += 1;
+        },
+      },
+      wrongCounter: {
+        element: (() => {
+          const newEl = document.createElement("p");
+          newEl.textContent = "Неправильно введённых слов : ";
+          const number = document.createElement("span");
+          number.innerText = 0;
+          newEl.append(number);
+          this.gameContainer.append(newEl);
+          return number;
+        })(),
+        count: 0,
+        upNum() {
+          this.element.innerText = this.count += 1;
+        },
+      },
+      wordsBlock: {
+        element: (() => {
+          const block = document.createElement("div");
+          this.gameContainer.append(block);
+          block.onclick = () => {
+            this.interface.wordsBlock.element.onclick = null;
+            this.interface.wordsBlock.element.innerText = "";
+            this.interface.wordsBlock.newWord();
+            //main game function
+            document.addEventListener("keyup", (event) => {
+              if (event.code.includes("Key")) {
+                console.log(event.key);
+                let letter = this.interface.wordsBlock.arrOfLetters[this.interface.wordsBlock.curentLettter]
+                if(letter.innerText === event.key) {
+                  letter.style.color = "green"
+                  this.interface.wordsBlock.curentLettter+=1
+                } else {
+                  letter.style.color = "red"
+                }
+              } else {
+                return false;
+              }
+            });
+          };
+          //CSS
+          block.innerText = "Start Game";
+          block.style.cursor = "pointer";
+          block.style.display = "inline-block";
+          block.style.padding = "5px";
+          block.style.fontSize = "50px";
+          block.style.border = "3px solid #888";
+          block.style.borderRadius = "3px";
+          block.style.backgroundColor = "#ccc";
+          return block;
+        })(),
+        wordsArr: [
+          "bob",
+          "awesome",
+          "netology",
+          "hello",
+          "kitty",
+          "rock",
+          "youtube",
+          "popcorn",
+          "cinema",
+          "love",
+          "javascript",
+        ],
+        curentLettter : 0,
+        arrOfLetters : [],
+        newWord() {
+          this.deleteWord()
+          let splited =
+            this.wordsArr[
+              Math.floor(Math.random() * this.wordsArr.length)
+            ].split("");
+          
+          splited.forEach((e) => {
+            let newLetter = document.createElement("span");
+            newLetter.innerText = e;
+            this.element.append(newLetter);
+            this.arrOfLetters.push(newLetter);
+          });
+        },
+        deleteWord(){
+          this.arrOfLetters = [];
+          this.element.innerHTML = "";
+          this.curentLettter = 0;
+        }
+      },
+    };
   }
 }
-
-new Game(document.getElementById('game'))
-
+const game = new KeyboardSoloGame();
